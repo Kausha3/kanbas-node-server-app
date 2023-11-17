f136bb8e6392d8aa0279d132dc43f71a8984f3c0
Kausha12 <trivedi.kau@northeastern.edu>
2023-11-17 00:31:55 -0500

    Assignment Module completed

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import Lab5 from "./lab5.js";
 import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
+import AssignmentRoutes from './assignments/routes.js';
 import "dotenv/config";
 
 const app = express()
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
+AssignmentRoutes(app);
 Hello(app)
 Lab5(app);
 
